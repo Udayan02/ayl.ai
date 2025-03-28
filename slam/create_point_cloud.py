a5ecd0105f68f619e9c726b0a3aac06a35a8f30c
udayan <udayansharma02@gmail.com>
2025-03-28 01:09:05 -0700

    2D point cloud tracks camera for better spatial understanding of point cloud

--- slam/create_point_cloud.py
+++ slam/create_point_cloud.py
@@ -3,10 +3,20 @@ import open3d as o3d
 import os
 import matplotlib.pyplot as plt
 
+
+def process_trajectory_file(file_path=None):
+    data = np.loadtxt(file_path)
+    if data.size == 0:
+        return None, None
+    timestamps = data[:, 0]
+    positions = data[:, 1:4]
+    return timestamps, positions
+
 # Dictionary to store points by timestamp
 points_by_time = {}
 
 folder = input("Enter the path to the tracked_keypoints directory: ")
+trajectory_file = input("Enter the path to the trajectory file: ")
 
 # Read all tracked keypoint files
 for filename in os.listdir(folder):
@@ -27,20 +37,29 @@ all_points = np.vstack([pts for pts in points_by_time.values()])
 pcd = o3d.geometry.PointCloud()
 pcd.points = o3d.utility.Vector3dVector(all_points)
 
-# Visualize
-o3d.visualization.draw_geometries([pcd])
-
 # Save the point cloud
 o3d.io.write_point_cloud("store_map.ply", pcd)
 
+# Get trajectory data
+timestamps, positions = process_trajectory_file(file_path=trajectory_file)
 
-# 2D Visualization (X-Z plane)
-plt.figure(figsize=(10, 8))
-plt.scatter(all_points[:, 0], all_points[:, 2], s=1, alpha=0.5)
-plt.title('2D Point Cloud (X-Z Plane)')
-plt.xlabel('X axis')
-plt.ylabel('Z axis')
+# Combined 2D Visualization (X-Z plane)
+plt.figure(figsize=(12, 10))
+
+# Plot point cloud
+plt.scatter(all_points[:, 0], all_points[:, 2], s=1, alpha=0.3, color='gray', label='Point Cloud')
+
+# Plot trajectory on top
+if timestamps is not None and positions is not None:
+    plt.plot(positions[:, 0], positions[:, 2], 'b-', linewidth=2, label='Trajectory')
+    plt.plot(positions[0, 0], positions[0, 2], 'go', markersize=8, label='Start')
+    plt.plot(positions[-1, 0], positions[-1, 2], 'ro', markersize=8, label='End')
+
+plt.title('2D Point Cloud with Trajectory (X-Z Plane)')
+plt.xlabel('X axis (meters)')
+plt.ylabel('Z axis (meters)')
 plt.axis('equal')  # Equal scale for X and Z axes
 plt.grid(True)
-plt.savefig("store_map_2d_xz.png", dpi=300)
+plt.legend()
+plt.savefig("combined_map_2d_xz.png", dpi=300)
 plt.show()
