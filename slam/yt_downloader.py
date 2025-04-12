bd1c3caefa5a1dbcb45c6d699028fd43a3e36181
Rahul <udayansharma02@gmail.com>
2025-04-11 21:25:02 -0700

    Prompt user for url and output_path

--- slam/yt_downloader.py
+++ slam/yt_downloader.py
@@ -1,14 +1,30 @@
 import yt_dlp
+import argparse
 
 # URL of the YouTube video
-video_url = "https://youtube.com/clip/Ugkx1bs6Q7y1Pt1_y2N7L7fPCxjaI55cT7KJ?feature=shared"
+video_url = "https://www.youtube.com/watch?v=ZItUSNL5Ies"
+parser = argparse.ArgumentParser(description="Download yt video without audio")
+print(f"Downloading yt video at {video_url}. Specify input yt url with --url argument when running the script to run on a different video")
+parser.add_argument("--url", default=video_url, type=str)
+parser.add_argument("--output_path", required=True, type=str)
+args = parser.parse_args()
+
+if not args.output_path.endswith(".mp4"):
+    # TODO: Accept different formats in the future
+    raise ValueError("Output file must end with mp4")
 
 # Define options for yt-dlp
 ydl_opts = {
-    'format': 'bestvideo+bestaudio/best',  # Downloads best video and audio quality
-    'outtmpl': '%(title)s.%(ext)s',  # Output filename format
+    'format': 'bv[ext=mp4]/bv',  # Downloads best video quality but no audio
+    'outtmpl': args.output_path,
+    'postprocessors': [
+        {
+            'key': 'FFmpegVideoConvertor',
+            'preferedformat': 'mp4',
+        }
+    ]
 }
 
 # Download video
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([video_url])
+    ydl.download([args.url])
